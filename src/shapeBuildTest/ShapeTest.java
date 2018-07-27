adbf55599e70385aad43b7dd6360b8e4abf54295
ernestb94 <ernest.busico@academytrainee.com>
2018-07-27 11:39:43 +0100

    TDD

--- src/shapeBuildTest/ShapeTest.java
+++ src/shapeBuildTest/ShapeTest.java
@@ -1,23 +1,30 @@
 package shapeBuildTest;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+import shapeBuild.Circle;
 import shapeBuild.Rectangle;
-import shapeBuild.Shape;
 import shapeBuild.Triangle;
 
 public class ShapeTest {
 	
 	 @Test
-	 public void testPeriShape() {
-        Triangle t = new Triangle(0, 0, 0);
-		assertEquals("Suppose to Fail",t.periShape(12,12,12));
+	 public void testTrianglePeri() {
+        Triangle t = new Triangle(0.0, 0.0, 0.0);
+		assertEquals("Suppose to Fail",t.periShape());
 	}
 	
 	 @Test
-	public void testAreaShape() {
-		Rectangle r = new Rectangle(0,0);
-		assertEquals("Suppose to Fail",r.areaShape(5,10));
+	public void testRectanglePeri() {
+		Rectangle r = new Rectangle(0.0,0.0);
+		assertEquals("Suppose to Fail",r.areaShape());
+	}
+	@Test
+	public void testCircleArea() {
+		Circle c = new Circle(0.0);
+		assertEquals("Suppose to Fail",c.areaShape());
 	}
 
 }
